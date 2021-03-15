157c22aee70b16f251d17c40815149cec8825662
Grace Fallon <graceleefallon@gmail.com>
2021-03-15 17:16:34 +0000

    Modify interpretation functions for buffer use

--- 1-print_funcs.c
+++ 1-print_funcs.c
@@ -1,84 +1,116 @@
 #include "holberton.h"
 
 /**
-* printPerc - Print the % character to standard output
-* @perc: **************************************
-* @count: existing total number of characters printed
+* printPerc - Store the % sign character in an array in memory
+* @perc:unused, argument list
 *
-* Return: cumulative total number of characters printed
+* Return: pointer to two bits in memory, holds '%' and null byte
 */
-int printPerc(va_list perc, int count)
+char *printPerc(va_list perc)
 {
-	va_arg(perc, int);
-	_putchar('%');
-	count++;
-	return (count);
+	char *str;
+	(void) perc;
+	str = malloc(2);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	str[0] = '%';
+	str[1] = '\0';
+	return (str);
 }
 
 /**
-* printc - Print ASCII character to standard output
+* printc - Convert ASCII value to character, store in an array in memory
 * @c: pointer to argument of type integer, (ASCII val)
-* @count: existing total number of characters printed
 *
-* Return: cumulative total number of characters printed
+* Return: pointer to two bits in memory, holds char and null byte
 */
-int printc(va_list c, int count)
+char *printc(va_list c)
 {
-	char cc;
-
-	cc = va_arg(c, int);
-	_putchar(cc);
-	count++;
-	return (count);
+	char *str;
+	char chr;
+	chr = va_arg(c, int);
+	str = malloc(2);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	str[0] = chr;
+	str[1] = '\0';
+	return (str);
 }
 
 /**
-* prints - Print string literal to standard output
+* prints - Copy string literal argument to allocated memory
 * @s: pointer to argument of type string literal
-* @count: existing total number of characters printed
 *
-* Return: cumulative total number of characters printed
+* Return: pointer to string in memory
 */
-int prints(va_list s, int count)
+char *prints(va_list s)
 {
-	char *ss;
+	char *str, *tmpstr;
+	int n;
+	tmpstr = va_arg(s, char *);
+	str = malloc(_strlen(tmpstr) + 1);
+	if  (str == NULL)
+	{
+		return (NULL);
+	}
 
-	ss = va_arg(s, char *);
-	count = _puts(ss, count);
-	return (count);
+	for (n = 0; n <= _strlen(tmpstr); n++)
+	{
+		str[n] = tmpstr[n];	
+	}
+
+	return (str);
 }
 
 /**
 * printint - Conveting integer data to a string
 * @d: pointer to argument of type integer
 *
-* Return: pointer to string
+* Return: pointer to string of numeric characters
 */
 char *printint(va_list d)
 {
 	char *tmp;
-	int i, rem, n, num, len;
-	
+	int i, rem, n, num, len, x;
+/** iterator, last digit, initial integer, copy of initial integer, length of input, length for negatives */	
+
 	n = va_arg(d, int);
 	num = n;
 	len = 0;
-        
-	while (n != 0)
+	i = 0;
+
+	while (n != 0)	/** Integer length */
 	{
 		len++;
 		n /= 10;
 	}
-	tmp = malloc(len + 1);
-	for (i = 0; i < len; i++)
+	x = len;
+	if (num = 0) /** Zero case */
 	{
-		rem = num % 10;
-		num /= 10;
-		tmp[len - (i + 1)] = (rem + '0');
+		tmp = malloc(2);
+		tmp[0] = '0';
 	}
-	tmp[len] = '/0';
+	else if (num < 0) /** Negative case */
+	{
+		x++ /** Account for dash indicator */
+		tmp = malloc(len + 2); /** Dash indicator and null byte */
+		tmp[0] = '-';
+		num *= -1;
+	}
+	else /** Positive case */
+	{
+		tmp = malloc(len + 1);
+	}
+	for (; i < len; i++)
+	{
+		rem = num % 10; /** Isolate last digt */
+		num /= 10; /** Shorten integer */
+		tmp[x - (i + 1)] = (rem + '0'); /** Fill, right to left */
+	}
+	tmp[x] = '\0';
 	return (tmp);
 }
-
-
-
-
